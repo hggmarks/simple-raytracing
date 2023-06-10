1331190fbb77b37fd840a871aacaa7cbf6a42505
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2023-06-10 11:58:13 -0300

    Delete target directory

--- target/debug/deps/simple_raytracing-1dcf0706ea740859.d
+++
@@ -1,6 +0,0 @@
-/mnt/c/Users/Hugo/Projects/simple-raytracing/target/debug/deps/simple_raytracing-1dcf0706ea740859.rmeta: src/main.rs src/vec3.rs
-
-/mnt/c/Users/Hugo/Projects/simple-raytracing/target/debug/deps/simple_raytracing-1dcf0706ea740859.d: src/main.rs src/vec3.rs
-
-src/main.rs:
-src/vec3.rs:
